6454d68d247bdb705e7a20e8d5a33c086b9197e0
erickflores430 <erickcarpinteyro@triton.edu>
2025-10-26 20:31:45 -0500

    Add files via upload

---
+++ PS1P3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	// define variable
+	string lname;
+	int score;
+
+	// input phase
+	cout << "Enter your last name: ";
+	cin >> lname;
+	cout << "Enter your score: ";
+	cin >> score;
+
+	// process phase 
+	cout << lname << " has a score of " << score;
+
+
+} // enf of main
